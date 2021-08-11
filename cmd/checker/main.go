be05e40c8a7d8e0eb7129652ea1183a04c2437b2
paavo <shibaev.pp@phystech.edu>
2021-08-11 16:46:55 +0300

    add responder

--- cmd/checker/main.go
+++ cmd/checker/main.go
@@ -15,6 +15,7 @@ import (
 
 func main() {
 	key := os.Args[1]
+	username := os.Args[2]
 	var config client.Config
 	config.ApiKey = key
 	alertCli, err := alert.NewClient(&config)
@@ -37,25 +38,14 @@ func main() {
 		for _, ch := range checkers {
 			if err = ch.AssertRunning(); err != nil {
 				log.Println(err)
-				req1 := alert.ListAlertRecipientRequest{}
-				resp, err := alertCli.ListAlertRecipients(nil, &req1)
-				recipient := resp.AlertRecipients[0]
-				if err != nil {
-					log.Panic(err)
-				}
 				responder := alert.Responder{
 					Type:     alert.UserResponder,
-					Id:       recipient.User.ID,
-					Username: recipient.User.Username,
+					Username: username,
 				}
 				req := alert.CreateAlertRequest{
 					Message:    err.Error(),
 					Priority:   alert.P1,
 					Responders: []alert.Responder{responder},
-					//User: alert.User{
-					//	ID:       "",
-					//	Username: "",
-					//},
 				}
 				if _, err := alertCli.Create(context.Background(), &req); err != nil {
 					log.Panic(err)
