8ca4090fe339f9d2e8439e186bd9c146f3f677d2
paavo <shibaev.pp@phystech.edu>
2021-08-15 14:09:46 +0300

    clean up

--- cmd/checker/main.go
+++ cmd/checker/main.go
@@ -11,31 +11,19 @@ import (
 )
 
 const (
-	// lamp createAlert --message "host down" --config lamp.conf
-	//--users pavel.sh@p2p.org --priority P1 --teams Tezos-Team
-	lamp = "lamp"
-	cmd  = "lamp createAlert --message %s --apiKey %s " +
+	cmd = "lamp createAlert --message %s --apiKey %s " +
 		"--users %s --priority %s"
 )
 
 func sendAlert(key, user, message, priority string) (err error) {
 	run := fmt.Sprintf(cmd, message, key, user, priority)
-	// log.Println(run)
 	_, err = exec.Command("bash", "-c", run).Output()
-	// log.Println(string(output))
 	return err
 }
 
 func main() {
 	key := os.Args[1]
 	username := os.Args[2]
-	//var config client.Config
-	//config.ApiKey = key
-	// alertCli, err := alert.NewClient(&config)
-	//if err != nil {
-	//	sendAlert(key, username, err.Error(), "P1")
-	//	return
-	//}
 	nodeChecker, err := checker.NewNodePortChecker()
 	if err != nil {
 		if err = sendAlert(key, username, err.Error(), "P1"); err != nil {
@@ -62,31 +50,3 @@ func main() {
 		}
 	}
 }
-
-//
-//func sendAlert(username string, err error, alertCli *alert.Client) {
-//	//responder := alert.Responder{
-//	//	Type: alert.UserResponder,
-//	//	Name: username,
-//	//}
-//	req := alert.CreateAlertRequest{
-//		Message:  err.Error(),
-//		Priority: alert.P1,
-//		User:     username,
-//		//Responders: []alert.Responder{responder,
-//		//	{
-//		//		Type: alert.TeamResponder,
-//		//		Name: "Tezos-Team",
-//		//	},
-//		//},
-//		Responders: [] alert.Responder{
-//			{Type: alert.UserResponder, Username: username},
-//			{Type: alert.TeamResponder, Name: username},
-//		},
-//	}
-//	resp, err := alertCli.Create(context.Background(), &req)
-//	if err != nil {
-//		log.Panic(err)
-//	}
-//	log.Println(resp.Result)
-//}
